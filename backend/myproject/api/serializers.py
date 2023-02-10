c5e6da2aa3754a40eda13f555f2e419caa0e3fa2
levopeti <leviskocka@gmail.com>
2023-02-10 20:23:49 +0100

    refresh repo

--- backend/myproject/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from base.models import Item
-
-
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = Item
-        fields = '__all__'
-
